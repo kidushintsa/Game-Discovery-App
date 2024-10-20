c8a7aaaeb49540904a232b4780aad863c525746a
kidus hintsa <kidus908@gmail.com>
2024-10-20 21:14:44 +0300

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+// import React from 'react'
+import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <div className='grid grid-cols-2 bg-slate-600'>
-      <div className='bg-amber-400 h-20 col-span-2'>Nav</div>
-      <div className='bg-lime-500 h-20 hidden lg:block'>Side</div>
-      <div className='bg-blue-700 h-20 col-span-2 lg:col-span-1'>Main</div>
+      <NavBar/>
+      <div className='bg-lime-500 hidden lg:block'>Side</div>
+      <div className='bg-blue-700 col-span-2 lg:col-span-1'>Main</div>
     </div>
   )
 }
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import logo from "../assets/glogo.jpg"
+const NavBar = () => {
+  return (
+    <nav className='col-span-2 flex'>
+      <div className="border flex p-2">
+        <img src={logo} alt="logo" className="w-11 md:w-16 lg:w-16 rounded-3xl"/>
+        <h5 className="my-auto ms-1">Nav</h5>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
