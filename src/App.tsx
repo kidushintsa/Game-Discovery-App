65fd60a829b6a83a5a234b22788201944b4043f9
kidus hintsa <kidus908@gmail.com>
2024-10-20 20:12:00 +0300

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Game</div>
+    <div className='grid grid-cols-2 bg-slate-600'>
+      <div className='bg-amber-400 h-20 col-span-2'>Nav</div>
+      <div className='bg-lime-500 h-20 hidden lg:block'>Side</div>
+      <div className='bg-blue-700 h-20 col-span-2 lg:col-span-1'>Main</div>
+    </div>
   )
 }
 
