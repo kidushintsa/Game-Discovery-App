8d2eb69bebd3120bc7beaf40b6178ef1f7927b39
kidus hintsa <kidus908@gmail.com>
2025-03-07 17:44:01 +0300

    add placeholder image to cards

--- src/services/ImageCrop.ts
+++ src/services/ImageCrop.ts
@@ -1,5 +1,6 @@
-
+import noImage from "../assets/glogo.jpg"
 const ImageCrop = (url:string, length:number, width:number) => {
+  if(!url) return noImage
   const target = "media/";
   const index = url.indexOf(target) + target.length
   return url.slice(0, index) +  `crop/${length}/${width}/`+ url.slice(index)
