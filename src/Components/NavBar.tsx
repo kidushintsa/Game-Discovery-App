9897316e45056b32ed7dc180be1079ac411700d3
kidus hintsa <kidus908@gmail.com>
2025-03-23 19:35:04 +0300

    fix navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,13 @@
 // import React from 'react'
+import { Link } from "react-router-dom";
 import logo from "../assets/glogo.jpg"
 import SearchInput from "./SearchInput"
 const NavBar = () => {
   return (
     <nav className="flex">
       <div className="flex p-2">
-        <img src={logo} alt="logo" className="w-11 md:w-16 lg:w-16 rounded-3xl" />
+        <Link to={"/"}>
+        <img src={logo} alt="logo" className="w-11 md:w-16 lg:w-16 rounded-3xl" /></Link>      
         <h5 className="my-auto ms-1 font-poppins text-white hidden md:block">
           Game Hub
         </h5>
--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -1,16 +1,21 @@
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useQueryStore from "../State-management/useQueryStore";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   const setSearchText = useQueryStore(s => s.setSearchText)//only rerender when the search text is changed
   return (
     <form
       className=" border-slate-500 rounded-full flex !bg-slate-700 h-1/2 self-center w-1/2 mx-auto px-2"
       onSubmit={(e) => {
         e.preventDefault();
-        if(ref.current) setSearchText(ref.current?.value);
+        if(ref.current) {
+          setSearchText(ref.current?.value)
+          navigate("/");
+        };
       }}
     >
       <BsSearch
